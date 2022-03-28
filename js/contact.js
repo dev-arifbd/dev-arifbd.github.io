dc3f267be7cddd7f56f71088bd76e410bf6c8aef
Mohammad Ariful Islam <78550135+dev-arifbd@users.noreply.github.com>
2022-03-28 12:05:01 +0600

    Update contact form with sweetalert2

--- js/contact.js
+++ js/contact.js
@@ -14,33 +14,23 @@ $(function () {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "form/contact.php";
+            var url = "https://formsubmit.co/mdarifulislam3579@gmail.com";
 
             // POST values in the background the the script URL
             $.ajax({
                 type: "POST",
                 url: url,
                 data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
+                success: Swal.fire(
+                    'Thank you!',
+                    'For send me a Message',
+                    'success'
+                  )
+              
+                
+                
             });
+            $('#contact-form')[0].reset();
             return false;
         }
     })
